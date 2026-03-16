13c9634fdad57d1d7b351a50d9ac2119929d91da
dbstj589 <lysfox01@gmail.com>
2026-03-16 11:03:03 +0900

    add: linux용 실행 파일 make.sh 추가

--- make.sh
+++ make.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
-
 set -e
 
-CSV_INPUT="${1:-input-data/stkg/KG1.csv}"
-FACTS_OUT="yago-data/03-stkg-facts.tsv"
+CSV="input-data/new_csv/KG3_HX_lat_relX_relCH2_ver2.0.csv"
+OUTDIR="yago-data/KG3"
 
-echo "Running STKG pipeline with input: $CSV_INPUT"
-date +"  Start time: %F %T"
+echo "Running STKG pipeline..."
 
-python3 01-make-schema.py
-python3 02-make-taxonomy.py
-python3 03-make-facts.py --in "$CSV_INPUT" --out "$FACTS_OUT"
-python3 04-make-typecheck.py
-python3 05-make-ids.py
-python3 06-make-statistics.py
-python3 07-export.py
+python3 01-make-schema.py --outdir "$OUTDIR"
+python3 02-make-taxonomy.py --outdir "$OUTDIR"
+python3 03-make-facts.py --in "$CSV" --out "$OUTDIR/03-stkg-facts.tsv"
+python3 04-make-typecheck.py --outdir "$OUTDIR"
+python3 05-make-ids.py --outdir "$OUTDIR"
+python3 06-make-statistics.py --outdir "$OUTDIR"
+python3 07-export.py --outdir "$OUTDIR"
 
-echo "STKG pipeline finished."
-date +"  End time: %F %T"
+echo "Pipeline finished."
